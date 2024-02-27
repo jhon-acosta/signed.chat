fc5ac584f52f647e778af56daaa1b5ff41abe278
AndyIsraelYanez <aiy.delacruz@yavirac.edu.ec>
2024-02-26 22:17:41 -0500

    update: funcionalidad se validar si usuario es repetido aparece un nuevo campo y se desaparece el de los avatars

--- components/CreateUser.tsx
+++ components/CreateUser.tsx
@@ -1,10 +1,8 @@
-
-
 import { useState, useEffect } from "react";
 import { axiosApp } from "@/lib/utils";
-import { SendOutlined } from "@ant-design/icons";
+import { SendOutlined, UploadOutlined } from "@ant-design/icons";
 import { UsuarioChat } from "@/types/UsuariosChat";
-import { Avatar, Button, Card, Form, Input, Select, message } from "antd";
+import { Avatar, Button, Card, Form, Input, Select, Upload, message } from "antd";
 import getIO from "@/lib/socket-io";
 
 const avatars = [
@@ -29,27 +27,28 @@ const avatars = [
   "https://d2u8k2ocievbld.cloudfront.net/memojis/male/12.png",
   "https://d2u8k2ocievbld.cloudfront.net/memojis/female/12.png",
 ];
-
 const CreateUser = () => {
   const [form] = Form.useForm<UsuarioChat>();
   const [isLoading, setIsLoading] = useState(true);
-  const [isSocketConnected, setIsSocketConnected] = useState(false); // Estado para controlar la conexión del socket
+  const [isSocketConnected, setIsSocketConnected] = useState(false);
+  const [existingUser, setExistingUser] = useState(false);
+  const [isPrivateKey, setIsPrivateKey] = useState(false);
   const loadingImage = "loading.gif";
 
   useEffect(() => {
-    const socket = getIO(); // Obtener el socket
+    const socket = getIO();
     socket.on("connect", () => {
       console.log("Connected to socket");
-      setIsSocketConnected(true); // Cambiar el estado cuando se conecte el socket
-      setIsLoading(false); // Cambiar el estado de carga a falso
+      setIsSocketConnected(true);
+      setIsLoading(false);
     });
     socket.on("disconnect", () => {
       console.log("Disconnected from socket");
-      setIsSocketConnected(false); // Cambiar el estado cuando se desconecte el socket
+      setIsSocketConnected(false);
     });
 
     return () => {
-      socket.disconnect(); // Desconectar el socket al desmontar el componente
+      socket.disconnect();
     };
   }, []);
 
@@ -60,24 +59,42 @@ const CreateUser = () => {
         "/v1/publico/usuarios",
         values
       );
+
       localStorage.setItem("currentUser", JSON.stringify(response.data));
-      message.success(`Usuario creado correctamente`);
+      message.success("Usuario nuevo creado. Bienvenido al chat.");
+
       await axiosApp.post(`/v1/publico/usuarios/${response.data._id}/estado`, {
         estado: "online",
       });
+
+      setExistingUser(true);
+      setIsPrivateKey(true);
     } catch (error) {
       console.error(error);
+      const existingUserResponse = await axiosApp.get<UsuarioChat[]>(
+        `/v1/publico/usuarios?username=${values.username}`
+      );
+
+      if (existingUserResponse.data.length > 0) {
+        setExistingUser(true);
+        setIsPrivateKey(true);
+
+        // Mostrar mensaje solo si es un usuario existente
+        message.warning("Usuario ya existe. Carga la clave privada para reanudar.");
+      } else {
+        message.error("Error al crear el usuario.");
+      }
     } finally {
       setIsLoading(false);
     }
-  };
+  }; 
 
   return (
     <div className="flex justify-center items-center w-full">
-      {isLoading ? ( // Mostrar GIF de carga mientras se conecta al socket
+      {isLoading ? (
         <div>
-         <img src={loadingImage} alt="Cargando..." />
-      </div>
+          <img src={loadingImage} alt="Cargando..." />
+        </div>
       ) : (
         <Card title="Genera un usuario" className="w-96">
           <Form
@@ -91,7 +108,7 @@ const CreateUser = () => {
             labelCol={{ span: 6 }}
             autoComplete="off"
             onFinish={onSubmit}
-            disabled={!isSocketConnected} // Deshabilitar el formulario si el socket no está conectado
+            disabled={isLoading}
           >
             <Form.Item
               label="Usuario"
@@ -101,33 +118,67 @@ const CreateUser = () => {
             >
               <Input placeholder="Ingresa un usuario" />
             </Form.Item>
-            <Form.Item
-              label="Avatar"
-              name="avatar"
-              rules={[{ required: true, message: "Selecciona un avatar" }]}
-            >
-              <Select
-                className="w-10"
-                options={avatars.map((avatar, index) => ({
-                  label: (
-                    <>
-                      {index + 1}: <Avatar src={avatar} />
-                    </>
-                  ),
-                  value: avatar,
-                }))}
-              />
-            </Form.Item>
-            <div className="flex justify-center items-center">
-              <Button
-                type="primary"
-                htmlType="submit"
-                icon={<SendOutlined />}
-                loading={isLoading}
+
+            {existingUser ? (
+              isPrivateKey ? (
+                <Form.Item
+                label="Carga Clave Privada"
+                name="privateKey"
+                valuePropName="fileList"
+                getValueFromEvent={(e: any) => e.fileList}
+                rules={[
+                  {
+                    required: true,
+                    message: "Carga tu archivo de clave privada (.pem)",
+                  },
+                ]}
               >
-                Comenzar chat
-              </Button>
-            </div>
+                <Upload beforeUpload={() => false} accept=".pem">
+                  <Button icon={<UploadOutlined />}>Seleccionar archivo</Button>
+                </Upload>
+              </Form.Item>
+              ) : (
+                <Form.Item
+                  label="Avatar"
+                  name="avatar"
+                  rules={[{ required: true, message: "Selecciona un avatar" }]}
+                >
+                  <Input type="file" accept=".png, .jpg, .jpeg" />
+                </Form.Item>
+              )
+            ) : (
+              <Form.Item
+                label="Avatar"
+                name="avatar"
+                rules={[{ required: true, message: "Selecciona un avatar" }]}
+              >
+                <Select
+                  className="w-10"
+                  options={avatars.map((avatar, index) => ({
+                    label: (
+                      <>
+                        {index + 1}: <Avatar src={avatar} />
+                      </>
+                    ),
+                    value: avatar,
+                  }))}
+                />
+              </Form.Item>
+            )}
+
+            {/* Mostrar el botón de enviar solo si el usuario no existe o si existe y se cargó la clave privada */}
+            {(existingUser && isPrivateKey) || !existingUser ? (
+              <div className="flex justify-center items-center">
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  icon={<SendOutlined />}
+                  loading={isLoading}
+                >
+                  Comenzar chat
+                </Button>
+              </div>
+            ) : null}
           </Form>
         </Card>
       )}
