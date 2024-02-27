adbf2d0a66f258f87e01e60d6176f7245f8c1e9e
Jhon Acosta <jhonerick2018@gmail.com>
2024-02-26 23:03:36 -0500

    feat(chat): Mejotas en componente

--- components/layouts/ProviderLayout.tsx
+++ components/layouts/ProviderLayout.tsx
@@ -1,25 +1,20 @@
 "use client";
-import axios from "axios";
 import getIO from "@/lib/socket-io";
+import ChatPage from "../SignedChat";
 import CreateUser from "../CreateUser";
-import { axiosApp } from "@/lib/utils";
 import Sider from "antd/es/layout/Sider";
+import { Socket } from "socket.io-client";
 import { UsuarioChat } from "@/types/UsuariosChat";
 import { Menu, Layout, Avatar, Badge } from "antd";
+import { axiosApp, getUsuarioLocalStg } from "@/lib/utils";
 import { ItemType, MenuItemType } from "antd/es/menu/hooks/useItems";
 import { FC, useMemo, useState, useEffect, PropsWithChildren } from "react";
 
 const ProviderLayout: FC<PropsWithChildren> = (props) => {
+  const [io, setIO] = useState<Socket>();
+  const [currentUser, setCurrentUser] = useState<UsuarioChat>();
+  const [receiverUser, setReceiverUser] = useState<UsuarioChat>();
   const [usersOnline, setUsersOnline] = useState<UsuarioChat[]>([]);
-  const [receiverUser, setReceiverUser] = useState<string | null>(null);
-  const [currentUser, setCurrentUser] = useState<UsuarioChat | null>(null);
-
-  const getUserLocalStorage = () => {
-    const currentUser = localStorage.getItem("currentUser");
-    setCurrentUser(
-      !currentUser ? null : (JSON.parse(currentUser || "{}") as UsuarioChat)
-    );
-  };
 
   const items = useMemo((): ItemType<MenuItemType>[] => {
     const data = usersOnline
@@ -38,7 +33,7 @@ const ProviderLayout: FC<PropsWithChildren> = (props) => {
           disabled: !!currentUser,
         } as never,
         {
-          key: currentUser._id,
+          key: "en-linea",
           icon: <Badge status={usersOnline.length > 0 ? "success" : "error"} />,
           label: `En línea (${usersOnline.length - 1})`,
           disabled: !!currentUser,
@@ -49,29 +44,30 @@ const ProviderLayout: FC<PropsWithChildren> = (props) => {
   }, [currentUser, usersOnline]);
 
   useEffect(() => {
-    getUserLocalStorage();
+    setCurrentUser(getUsuarioLocalStg());
+
     const getUsersOnline = async () => {
       try {
-        const response = await axiosApp.get<{ data: UsuarioChat[] }>(
+        const response = await axiosApp.get<UsuarioChat[]>(
           "/v1/publico/usuarios"
         );
-        setUsersOnline(response.data?.data || []);
+        setUsersOnline(response?.data || []);
+        if (response?.data.length > 2) setReceiverUser(response?.data[0]);
       } catch (error) {
         console.error("Error:", error);
       }
     };
-
     getUsersOnline();
 
     const io = getIO();
+    setIO(io);
     io.on("usuarios-online", (users: UsuarioChat[]) => {
-      console.log("users", users);
+      if (users.length > 2) setReceiverUser(users[1]);
       setUsersOnline(users);
       setTimeout(() => {
-        getUserLocalStorage();
+        setCurrentUser(getUsuarioLocalStg());
       }, 100);
     });
-
     return () => {
       io.disconnect();
     };
@@ -79,25 +75,31 @@ const ProviderLayout: FC<PropsWithChildren> = (props) => {
 
   return (
     <Layout className="!min-h-screen bg-[#0c1317]">
-      <Layout.Content className="flex px-40 py-20 rounded-3xl ">
+      <Layout.Content className="flex px-5 md:px-40 md:py-20 rounded-3xl ">
         {!currentUser ? (
           <CreateUser />
         ) : (
           <>
-            <Sider width={250}>
+            <Sider width={200} breakpoint="lg" collapsedWidth="0">
               <Menu
                 mode="inline"
                 theme="dark"
                 items={items}
                 style={{ height: "100%" }}
                 className="bg-[#202c35]"
-                onSelect={({ key }) => {
-                  setReceiverUser(key);
+                onSelect={(seleccionado) => {
+                  setReceiverUser(
+                    usersOnline.find((user) => user._id === seleccionado.key)
+                  );
                 }}
               />
             </Sider>
-            <Layout.Content className="bg-[#202c35] p-5">
-              {props.children}
+            <Layout.Content className="bg-[#202b30] ">
+              <ChatPage
+                io={io}
+                currentUser={currentUser}
+                receiverUser={receiverUser}
+              />
             </Layout.Content>
           </>
         )}
